0dccfbce3a6149d39fd947c3ca6326dec3a62268
Shashwat Goel <shashwat.stanford@gmail.com>
2022-01-30 10:58:25 +0530

    Updated new_page()

--- wiki/wiki/encyclopedia/views.py
+++ wiki/wiki/encyclopedia/views.py
@@ -29,4 +29,29 @@ def search(request):
     return render(request, "encyclopedia/search.html", {"results":results, "query":query, "res":len(results)!=0})
 
 def new_page(request):
-    return render(request, "encyclopedia/create.html")
+    if request.method == "POST":
+        t_form = NewTitleForm(request.POST)
+        c_form = NewPageForm(request.POST)
+        if c_form.is_valid() and t_form.is_valid():
+            content = c_form.cleaned_data["page_entry"]
+            title = t_form.cleaned_data["page_title"]
+            if title not in util.list_entries():
+                util.save_entry(title, content)
+                return render(request, "encyclopedia/entry.html", {"content":content, "title": title.upper()})
+            else:
+                return render(request, "encyclopedia/create.html", {"red": True})
+        else:
+            return render(request, "encyclopedia/create.html", {"textarea": NewPageForm(), "title": NewTitleForm()})
+    return render(request, "encyclopedia/create.html", {
+        "textarea": NewPageForm(),
+        "title": NewTitleForm(),
+        "red": None,
+        })
+
+def edit_page(request, title):
+    content=util.get_entry(title)
+    if request.method == "POST":
+        new_content = request.POST.get("content")
+        util.save_entry(title, new_content)
+        return HttpResponseRedirect(reverse("encyclopedia:entry", kwargs={'title': title}))
+    return render(request, "encyclopedia/edit.html", {"title":title, "content":content})
