2d399285229befe737c355cdb0d2de1b7cf11f10
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-04-15 18:38:29 +0530

    Starting C++

---
+++ C++/first.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+string name;
+int x;
+int y;
+int z;
+
+int main()
+{
+    /*
+    cout << "Number 1: ";
+   cin >> x;
+   cout << "Number2: ";
+   cin >> y;
+   z = x | y;
+   cout << x << " | " << y << " equals " << z;
+    */
+   
+    cout << "Name: ";
+    getline (cin, name);
+    cout << "Hi, " << name << " \nYour name is " << name.size() << " characters long";
+    name[0] = '\nP';
+    cout << name;
+   
+   return 0;
+}
