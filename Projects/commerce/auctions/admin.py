bfccef3fa14ea5db2542f3ae2717b44ba4c5f01e
Shashwat <shashwat.stanford@gmail.com>
2022-04-23 12:04:33 +0530

    Commerce

--- Projects/commerce/auctions/admin.py
+++ Projects/commerce/auctions/admin.py
@@ -4,4 +4,4 @@ from .models import *
 # Register your models here.
 admin.site.register(Listing)
 admin.site.register(Category)
-admin.site.register(Bid)
+admin.site.register(User)
--- Projects/commerce/auctions/models.py
+++ Projects/commerce/auctions/models.py
@@ -5,19 +5,12 @@ from django.db import models
 class User(AbstractUser):
     pass
 
-class Bid(models.Model):
-    user = models.CharField(max_length=50)
-    bid = models.IntegerField()
-    listing_title = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f"Bid of {self.listing_title} of {self.bid} by {self.user}"
 
 class Listing(models.Model):
     title = models.CharField(max_length=128)
     description = models.CharField(max_length=1000)
     bid = models.IntegerField()
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='lisitings')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
     image=models.ImageField(upload_to='media/', blank=True)
 
     def __str__(self):
