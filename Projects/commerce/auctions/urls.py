86289c86a9b54333b11329f8e9f5e5bdd7f88f04
Shashwat <shashwat.stanford@gmail.com>
2022-04-25 14:26:44 +0530

    Minor Changes and reversions

--- Projects/commerce/auctions/urls.py
+++ Projects/commerce/auctions/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from .models import *
 
+listings = [str(listing.id) for listing in Listing.objects.all()]
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -13,8 +15,9 @@ urlpatterns = [
     path("categories", views.categories, name="categories"),
     path("watchlist", views.watchlist, name="watchlist"),
     path("<int:listing_id>", views.listing, name="listing"),
-    path("category", views.category, name="category"),
 ]
+
+
 
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
