c27b2278a2081b3f9be36f852249cf961ece38f1
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-04-29 11:38:23 +0530

    Used re_path for only users and posts. FIxed bug for media files

--- Projects/project4/network/urls.py
+++ Projects/project4/network/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path, re_path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+from .models import *
+
+users = f"({('|').join([user.username for user in User.objects.all()])})"
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -12,6 +16,11 @@ urlpatterns = [
     path("following", views.following, name="following"),
 
     #Regex Paths
-    re_path(r'(\d+)', views.likes, name="likes"), #(API Route)
-    re_path(r'(\w+)', views.profile, name="profile")
+    re_path(r'posts/(\d+)', views.likes, name="likes"), #(API Route)
+    re_path(users, views.profile, name="profile")
 ]
+
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
+
