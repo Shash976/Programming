fa2d8e90d5056f344f14177c950c0378922204ab
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-04-29 11:38:58 +0530

    Added default values for 'time' and 'likes' field

--- Projects/project4/network/models.py
+++ Projects/project4/network/models.py
@@ -16,8 +16,8 @@ class Post(models.Model):
     content = models.CharField(max_length=250)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     image = models.ImageField(upload_to='media/', blank=True)
-    time = models.DateTimeField()
-    likes = models.IntegerField()
+    time = models.DateTimeField(default=datetime.datetime.now())
+    likes = models.IntegerField(default=0)
 
     def __str__(self):
         return f"Post by {self.user} at {self.time}"    
