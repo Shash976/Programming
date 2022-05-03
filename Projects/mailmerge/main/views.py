0f25408c1fc5eae67b13e1a0578fbdb36b0af3fb
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-05-03 15:16:31 +0530

    Added Login/Logout and Register features

--- Projects/mailmerge/main/views.py
+++ Projects/mailmerge/main/views.py
@@ -1,33 +1,63 @@
-from django.shortcuts import render
-from django.urls import reverse
-from django.http import HttpResponseRedirect
-
-from .forms import NewEmailForm, NewRecipientForm
-from .models import Recipient
-from .convert import *
-
+import json
+import os
 import re
 import smtplib
-import json
+
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+
+from .convert import *
+from .forms import *
+from .models import *
+
 
 # Create your views here.
-'''
-def create_recipient(request):
-    if request.method == "POST":
-        form = NewRecipientForm(request.POST)
-        if form.is_valid():
-            first_name = form.cleaned_data["first_name"]
-            last_name = form.cleaned_data["last_name"]
-            address = form.cleaned_data["address"]
-            email = form.cleaned_data["email"]
-        new = Recipient.objects.create(first_name=first_name, last_name=last_name, address=address, email=email)
-        new.save
-        return HttpResponseRedirect(reverse("index"))
-    return render(request, "main/index.html", {
-        "form": NewRecipientForm(),
-        })
-'''
 
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "main/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "main/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "main/register.html", {"message": "Passwords must match."})
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+        except IntegrityError:
+            return render(request, "main/register.html", {"message": "Username already taken."})  
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "main/register.html")
 
 def index(request):
     if request.method == "POST":
