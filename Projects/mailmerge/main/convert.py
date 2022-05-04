db8f847a13838a0c28c0fb0a91525660ac4f86ed
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-05-04 11:06:02 +0530

    COnverting excel,csv files to JSON

--- Projects/mailmerge/main/convert.py
+++ Projects/mailmerge/main/convert.py
@@ -5,18 +5,32 @@ import sys
 
 path = sys.argv[1]
 
+'''---------------------------------------------------------------------------x------------------------------------------------x-------------------------------------------'''
+def excel_to_json(path):
+    csv_path= excel_to_csv(path)
+    return csv_to_json(csv_path)
+
 '''---------------------------------------------------------------------------x-------------------------------x--------------------------------------------------------------'''
+# read an excel file and convert 
+# into a dataframe object
+def excel_to_csv(filepath):
+    df = pd.DataFrame(pd.read_excel(filepath))
+    i = filepath.rindex('.')
+    csv_filepath = filepath[:i] + '_con.csv'
+    json_filepath = filepath[:i] + '_conj.json'
+    df.to_csv(csv_filepath, index=None, header=True)
+    return csv_filepath
+
+'''---------------------------------------------------------------------x-----------------------------------------------------------------x--------------------------------------'''
 
 # Function to convert a CSV to JSON
 # Takes the file paths as arguments
-def csv_to_json(csvFilePath, jsonFilePath):
-
+def csv_to_json(csvFilePath):
+    jsonFilePath = csvFilePath[:csvFilePath.rindex('n')] + 'j.json'
     data = []
-    # Open a csv reader called DictReader
     with open(csvFilePath, encoding='utf-8') as csvf:
-        csvReader = csv.DictReader(csvf)
+        csvReader = csv.DictReader(csvf) # Open a csv reader called DictReader
         '''Convert each row into a dictionary and add it to data'''
-        i = 1
         for row in csvReader:
              data.append(row)
 
@@ -26,23 +40,6 @@ def csv_to_json(csvFilePath, jsonFilePath):
     
     return jsonFilePath
 '''---------------------------------------------------------------------x-----------------------------------------------------------------x--------------------------------------'''
-# read an excel file and convert 
-# into a dataframe object
-def excel_to_csv(filepath):
-    df = pd.DataFrame(pd.read_excel(filepath))
-    i = filepath.rindex('.')
-    csv_filepath = filepath[:i] + '_con.csv'
-    json_filepath = filepath[:i] + '_conj.json'
-    df.to_csv(csv_filepath, index=None, header=True)
-    return csv_filepath, json_filepath
-
-'''---------------------------------------------------------------------x-----------------------------------------------------------------x--------------------------------------'''
-
-def excel_to_json(path):
-    tup = excel_to_csv(path)
-    csv_path = tup[0]
-    json_path = tup[1]
-    return csv_to_json(csv_path, json_path)
 
 if __name__ == "__main__":
     excel_to_json(path)
