029e31daf1bdf8ca090b0463131430f8c0dddd96
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-05-18 11:33:36 +0530

    Added Login /logout and register

--- Projects/capstone/battleship/models.py
+++ Projects/capstone/battleship/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import json
+
 
 # Create your models here.
+class User(AbstractUser):
+    hits = models.IntegerField(default=0)
+    turns = models.IntegerField(default=0)
--- Projects/capstone/battleship/views.py
+++ Projects/capstone/battleship/views.py
@@ -1,6 +1,55 @@
 from django.shortcuts import render
+from django.db import IntegrityError
+from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+
+from .models import *
 
 # Create your views here.
+def login_view(request):
+    if request.method == "POST":
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "battleship/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "battleship/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "battleship/register.html", {"message": "Passwords must match."})
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password)
+            user.save()
+        except IntegrityError:
+            return render(request, "battleship/register.html", {"message": "Username already taken."})  
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "battleship/register.html")
 
 def index(request):
     if request.method == "POST":
