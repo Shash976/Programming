b67b1c0d602ef72074e2b1ec231305de0ec40a54
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-05-18 11:24:05 +0530

    Data Science

--- PythonPrograms/DS/lrtable.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import statsmodels.formula.api as smf
-
-full_health_data = pd.read_csv("CSVs/data2.csv", header=0, sep=",")
-
-model = smf.ols('Calorie_Burnage ~ Average_Pulse', data = full_health_data)
-results = model.fit()
-print(results.summary())
