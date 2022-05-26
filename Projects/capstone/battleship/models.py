f88edb2dab4edca0a4b34f771d983600466db66b
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-05-26 10:22:40 +0530

    new through model 'PlayerInGame' added

--- Projects/capstone/battleship/models.py
+++ Projects/capstone/battleship/models.py
@@ -24,10 +24,38 @@ class Map(models.Model):
         return f"{self.user}\'s {x.number_to_words(x.ordinal(list(self.user.maps.all()).index(self)+1)).capitalize()} Map"
 
 class Match(models.Model):
-    player_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="matches_player1")
-    player_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name="matches_player2")
-    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="matches_won")
+    players = models.ManyToManyField(
+        User,
+        through='PlayerInGame',
+        through_fields=('match', 'user'),
+        related_name="matches"
+    )
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="matches_won", blank=True, null=True)
 
-    
     class Meta:
         verbose_name_plural = "Matches"
+    
+    def __str__(self):
+        return f"Match {self.id}"
+
+class PlayerInGame(models.Model):
+    WINNER = 'Winner'
+    LOSER = 'Loser'
+    UKNOWN = 'Unknown'
+    PLAYER_TYPE_CHOICES = [
+           (WINNER, 'Winner'),
+           (LOSER, 'Loser'),
+           (UKNOWN, 'Unknown')
+       ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    type = models.CharField(choices=PLAYER_TYPE_CHOICES, max_length=7, default="UNKNOWN")
+    turns = models.IntegerField(default=0)
+    hits = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = "InGame Players"
+    
+    def __str__(self):
+        return f"Player - {self.user} in game {self.match.id}"
