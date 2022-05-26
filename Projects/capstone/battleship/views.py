ca31ec097d4a4e35c5cd119f530935a50f522536
Shashwat Goel <62839096+Shash976@users.noreply.github.com>
2022-05-26 10:06:04 +0530

    imported JSON

--- Projects/capstone/battleship/views.py
+++ Projects/capstone/battleship/views.py
@@ -7,6 +7,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
 
+import json
+
 from .models import *
 
 # Create your views here.
@@ -19,7 +21,7 @@ def login_view(request):
         # Check if authentication successful
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect(reverse("index"))
+            return HttpResponseRedirect(reverse("battleship:index"))
         else:
             return render(request, "battleship/login.html", {
                 "message": "Invalid username and/or password."
@@ -27,10 +29,10 @@ def login_view(request):
     else:
         return render(request, "battleship/login.html")
 
-@login_required(login_url=reverse_lazy("login"))
+@login_required(login_url=reverse_lazy("battleship:login"))
 def logout_view(request):
     logout(request)
-    return HttpResponseRedirect(reverse("index"))
+    return HttpResponseRedirect(reverse("battleship:index"))
 
 def register(request):
     if request.method == "POST":
@@ -52,26 +54,50 @@ def register(request):
     else:
         return render(request, "battleship/register.html")
 
-@login_required(login_url=reverse_lazy("login"))
+@login_required(login_url=reverse_lazy("battleship:login"))
 def index(request):
     if request.method == "POST":
         pass
     return render(request, "battleship/index.html", {"players":User.objects.exclude(username=request.user.username).all()})
 
-@login_required(login_url=reverse_lazy("login"))
+def map_api(request, map_id):
+    map = Map.objects.get(id=map_id)
+    return JsonResponse(map.serialize())
+
+@login_required(login_url=reverse_lazy("battleship:login"))
 @csrf_exempt
 def create_map(request):
     if request.method == "POST":
         data = json.loads(request.body)
-        username = data.get("user")
+        username = data.get("player")
         map = data.get("map")
+        i = data.get("index")
         m = Map.objects.create(user=User.objects.get(username=username), map=map)
         m.save()
-        return JsonResponse({"message": "It worked successfully."}, status=201)
+        return JsonResponse({"message": f"Worked. Index is {i}"})
     else:
-        return JsonResponse({"error": "POST request required."}, status=400)
+        return JsonResponse({"message": "Please send POST request"})
 
 @login_required(login_url=reverse_lazy("login"))
 @csrf_exempt
-def play_game(request, player1, player2):
-    pass
+def play(request):
+    p1 = request.GET.get('p1').strip()
+    p1m = request.GET.get('p1m').strip()
+    p2 = request.GET.get('p2').strip()
+    p2m = request.GET.get('p2m').strip()
+    u1m = max([m.id for m in Map.objects.filter(map=(', ').join(p1m.split(',')))])
+    u2m = max([m.id for m in Map.objects.filter(map=(', ').join(p2m.split(',')))])
+    players = {"p1":p1,"p1m":u1m, "p2":p2, "p2m":u2m}
+    return HttpResponseRedirect(reverse("battleship:game", kwargs={"players":json.dumps(players)}))
+
+def game(request, players):
+    players = json.loads(players)
+    game = Match.objects.create()
+    game.save()
+    p1 = PlayerInGame.objects.create(user = User.objects.get(username=players["p1"]), match=game, type='UNKNOWN')
+    p2 = PlayerInGame.objects.create(user = User.objects.get(username=players["p2"]), match=game, type='UNKNOWN')
+    p1.save()
+    p2.save()
+    p1m = Map.objects.get(id=players["p1m"])
+    p2m = Map.objects.get(id=players["p2m"])
+    return render(request, "battleship/play.html", {"match": game})
